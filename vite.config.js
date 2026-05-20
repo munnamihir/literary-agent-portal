b865ecd9170a82b9dc95f81c88fb688ae6383829
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-05-20 16:06:24 -0400

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Change 'literary-agent-portal' to your actual GitHub repo name
-  base: '/literary-agent-portal/',
+  base: './',
 })
